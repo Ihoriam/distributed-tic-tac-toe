d21c23f680b9e7da369964d8a4fc80b4a9665959
Ihor Ihnatenko <igignt@gmail.com>
2026-01-27 19:00:35 +0200

    create project base

---
+++ src/main/java/org/ihor/distributedtictactoe/DistributedTicTacToeApplication.java
@@ -0,0 +1,13 @@
+package org.ihor.distributedtictactoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedTicTacToeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedTicTacToeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ihor/distributedtictactoe/DistributedTicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ihor.distributedtictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedTicTacToeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
