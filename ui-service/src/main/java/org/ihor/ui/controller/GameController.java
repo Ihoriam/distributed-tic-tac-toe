6bf01bcc7576528489959e03e8964e07120e8ae4
Ihor Ihnatenko <igignt@gmail.com>
2026-01-28 18:31:34 +0200

    Add UI application configuration, enhance game.html with move history and loading states

--- ui-service/src/main/java/org/ihor/ui/controller/GameController.java
+++ ui-service/src/main/java/org/ihor/ui/controller/GameController.java
@@ -1,13 +1,23 @@
 package org.ihor.ui.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GameController {
 
+    @Value("${game.session.base-url}")
+    private String sessionBaseUrl;
+
+    @Value("${game.session.ws-url}")
+    private String sessionWsUrl;
+
     @GetMapping("/")
-    public String game() {
+    public String game(Model model) {
+        model.addAttribute("sessionApiUrl", sessionBaseUrl);
+        model.addAttribute("wsUrl", sessionWsUrl);
         return "game";
     }
 }
